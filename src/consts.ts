335ba51f69dab15a67f84afc3218de9ccce7a7bd
KONFeature <quentin@nivelais.com>
2025-11-20 20:59:06 +0100

    Improve SEO

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,9 @@
 
 export const SITE_TITLE = 'Quentin Nivelais';
 export const SITE_DESCRIPTION = 'Co-Founder & CTO at Frak Labs. EVM infrastructure, DevOps, and Hardware.';
+export const AUTHOR_NAME = 'Quentin Nivelais';
+export const AUTHOR_EMAIL = 'quentin@nivelais.com';
+export const TWITTER_HANDLE = '@QNivelais';
+export const GITHUB_HANDLE = 'KONFeature';
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/quentin-nivelais';
+export const SITE_URL = 'https://nivelais.com';
